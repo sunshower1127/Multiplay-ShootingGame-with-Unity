ab45f6db8422ac60408c1c299c7025914f829641
Sunwoo Kim <sunshower1127@gmail.com>
2024-06-13 01:43:01 +0900

    First commit

---
+++ NetworkManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using TMPro;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public TMP_InputField NicknameInput;
+    public GameObject DisconnectPanel;
+    public GameObject RespawnPanel;
+    // Start is called before the first frame update
+    public void Awake()
+    {
+        Screen.SetResolution(960, 540, false);
+        PhotonNetwork.SendRate = 60;
+        PhotonNetwork.SerializationRate = 30;
+    }
+
+    public void Connect() { PhotonNetwork.ConnectUsingSettings(); }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.LocalPlayer.NickName = NicknameInput.text;
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 6 }, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        DisconnectPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.Disconnect();
+        }
+
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        DisconnectPanel.SetActive(true);
+        RespawnPanel.SetActive(false);
+    }
+}
